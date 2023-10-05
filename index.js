39f4cebdf04de969d53f140b6c0ef8e884e3f092
samuel <sammykuria300@gmail.com>
2023-10-05 14:01:05 +0300

    Quick commit

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email,
+    };
+  
+    return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // Access the new id from the response data
+        const newId = data.id;
+        // Append the new id to the DOM
+        document.body.innerHTML = newId;
+      })
+      .catch(function (error) {
+        // Handle errors by appending the error message to the DOM
+        document.body.innerHTML = error.message;
+      });
+  }
+  
+  // Call the submitData function with name and email values
+  submitData('John Doe', 'john@example.com');
+  
